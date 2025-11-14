d1dc93051845fac43615ffec5083bea29ce9a314
AnnS9 <75726606+AnnS9@users.noreply.github.com>
2025-11-14 17:35:15 +0000

    loader3

--- frontend/src/components/RouteLoader.jsx
+++ frontend/src/components/RouteLoader.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Loader from "Loader";
+import Loader from "./Loader";
 
 export default function RouteLoader() {
   const { pathname } = useLocation();
